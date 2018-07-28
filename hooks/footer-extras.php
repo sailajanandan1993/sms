0fb3ef1346b12454d1ed0fd0d0ce048dd9d3b696
Sailaja Nandan Satpathy <mailtosailaja3@gmail.com>
2018-07-28 16:22:41 +0530

    first commit

---
+++ hooks/footer-extras.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Version</b> 1.1.0
+    </div>
+    <strong> SMS &nbsp;<a href="#">Brought To You By Sailaja Nandan</a>.</strong>
+  </footer>
